29ce41486db18f06a63585771ee8d3cf3f8ea845
Michael Maitlen <mgmaitlen@gmail.com>
2019-08-08 15:34:15 -0700

    add in ui components

--- app/src/main/java/com/mgm/translator/MainActivity.kt
+++ app/src/main/java/com/mgm/translator/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mgm.translator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        am_btn_translate.setOnClickListener {
+            am_output.text = am_input.text.toString()
+        }
     }
 }
